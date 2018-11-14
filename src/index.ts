0ad82418a60c2c2c6b9ae2f15cb5dfb9416ca144
Quinntyne Brown <quinntynebrown@gmail.com>
2018-11-14 14:55:00 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-﻿//import "./auth.interceptor";
-//import "./auto-complete.component";
-//import "./constants";
-//import "./search-box.component";
-//import "./search-result-item-detail.component";
-//import "./search-result-item.component";
-//import "./search-result-items.component";
-//import "./header.component";
-import "./basic-repeat.component";
+﻿import "./auth.interceptor";
+import "./auto-complete.component";
+import "./constants";
+import "./search-box.component";
+import "./search-result-item-detail.component";
+import "./search-result-item.component";
+import "./search-result-items.component";
+import "./header.component";
