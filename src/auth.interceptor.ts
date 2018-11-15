1c89fa6d84b6e249626059b3b2f55cb614e58dae
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2018-11-15 02:38:04 -0500

    refactor

--- src/auth.interceptor.ts
+++ src/auth.interceptor.ts
@@ -1,6 +1,6 @@
-import * as fetchIntercept from 'fetch-intercept';
+import { register } from 'fetch-intercept';
 
-fetchIntercept.register({
+register({
   request: function (url, config) {
     return [
       `${url}&access_key=MDo5ZjQzZjZjYS0zYzZkLTExZTctODVjZC1lNzY5OWY5Mzg2MjE6NEp5dmw3Y3ZIdEpxU29Ka2diYlhPanBXRFZhbXJyNWVIZ3VJ`,
